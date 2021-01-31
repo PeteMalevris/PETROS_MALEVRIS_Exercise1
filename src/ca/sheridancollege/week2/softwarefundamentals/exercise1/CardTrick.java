fc0a0c0c63e90d2c8fccb0ecdfa200461cff0a7e
Petros Malevris <55808329+PeteMalevris@users.noreply.github.com>
2021-01-31 16:26:37 -0500

    - 	Completed CardTrick class so that it models a "hand" of seven cards 	created at random -	Added function to ask user to pick a card by value and suit

--- src/ca/sheridancollege/week2/softwarefundamentals/exercise1/CardTrick.java
+++ src/ca/sheridancollege/week2/softwarefundamentals/exercise1/CardTrick.java
@@ -3,8 +3,15 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
+/*
+ * Petros Malevris
+ * Student #991247545
+ */
 package ca.sheridancollege.week2.softwarefundamentals.exercise1;
-
+import java.util.Arrays;
+import java.util.InputMismatchException;
+import java.util.Random;
+import java.util.Scanner;
 /**
  * A class that fills a magic hand of 7 cards with random Card Objects
  * and then asks the user to pick a card and searches the array of cards
@@ -16,17 +23,54 @@ public class CardTrick {
     public static void main(String[] args)
     {
         Card[] magicHand = new Card[7];
-
+        
         for (int i=0; i<magicHand.length; i++)
         {
+            //Random class to generate card values at random
+            Random value = new Random();
+            int valueBound = 14;
+            int randValue = value.nextInt(valueBound);
+            //Random class to generate card Suit at random
+            Random rSuit = new Random();
+            int suitBound = 4;
+            int randSuit = rSuit.nextInt(suitBound);
             Card c = new Card();
+            
             //c.setValue(insert call to random number generator here)
+            c.setValue(randValue);
             //c.setSuit(Card.SUITS[insert call to random number between 0-3 here])
+            c.setSuit(Card.SUITS[randSuit]);
+            System.out.println(c.getValue() + " of " + c.getSuit());
         }
-
         //insert code to ask the user for Card value and suit, create their card
+        System.out.println("Pick a card, any card "
+                            +"\nNumber: ");
+        Card user = new Card();
+        user.setValue(intInput());
+        System.out.println("Pick a suit (0 = Hearts, 1 = Diamonds, 2 = Spades, 3 = Clubs): ");
+        user.setSuit(Card.SUITS[intInput()]);
         // and search magicHand here
-        //Then report the result here
+        
+    }
+    
+    /**
+     * Prompts the user to enter a number and only a number and will cycle until
+     * a number has been given
+     * @return number
+     */
+    public static int intInput() {
+        Scanner keyboard = new Scanner(System.in);
+        int number = 0;
+        while (!keyboard.hasNextInt()) {
+            keyboard.next();
+            System.out.print("Please enter a number: ");
+        }
+        try {
+            number = keyboard.nextInt();
+        } catch (InputMismatchException exception) {
+            System.out.println("Only numbers are allowed.");
+        }
+        return number;
     }
 
 }
